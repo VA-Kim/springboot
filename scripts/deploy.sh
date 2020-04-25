0404fcbabe9a2115d0dd960184733e91631ab78e
Chris <chrisainerd@gmail.com>
2020-04-25 14:16:08 +0900

    fix 오타

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,9 +32,9 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME"
 
-nohub java -jar \
+nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 
