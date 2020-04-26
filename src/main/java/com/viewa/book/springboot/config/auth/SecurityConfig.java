c655a38392253b86e2e8b6c09c98f86850cb5a2d
Chris <chrisainerd@gmail.com>
2020-04-26 16:47:47 +0900

    add nginx

--- src/main/java/com/viewa/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/viewa/book/springboot/config/auth/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // 2. h2-console 화면을 사용하기 위해 해당 옵션들을 disable 함
                 .and()
                     .authorizeRequests()    // 3. URL별 권한 관리를 설정하는 옵션의 시작점 & authorizeRequests가 선언되어야만 antMatchers 옵션을 사용할 수 있음
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())    // 4. 권한 관리 대상을 지정하는 옵션 & URL, HTTP 메소드별로 관리가 가능함 & "/"등 지정된 URL들은 permitAll() 옵션ㅇ르 통해 전체 열람 권한을 주었음 & POST 메소드이면서 "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 구현
                     .anyRequest().authenticated()   // 5. 설정된 값들 이외 나머지 URL들을 나타냄 & 여기서느 authenticated()을 추가하여 나머지 URL들은 모두 인증된 사
                 .and()
